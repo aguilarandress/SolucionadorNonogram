5a63429f29ed3c51fd134b7bd37774ddce28b93a
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-03-01 20:11:20 -0600

    Boton para cambiar entre escenas

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    private Button loadNonogramBtn;
+    void Awake()
+    {
+        // Get btn
+        this.loadNonogramBtn = GameObject.Find("NonogramBtn").transform.Find("Button").GetComponent<Button>();
+        this.loadNonogramBtn.onClick.AddListener(this.loadInputFileScene);
+    }
+
+    private void loadInputFileScene()
+    {
+        // Cargar la escena
+        SceneManager.LoadScene("InputFileScene");
+    }
+}
