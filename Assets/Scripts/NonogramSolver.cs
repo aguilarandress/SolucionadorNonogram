ef78e1ba97d578839a6bc28a8b6d2a09a736855a
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-03-17 19:19:01 -0600

    Algoritmo optimizado

--- Assets/Scripts/NonogramSolver.cs
+++ Assets/Scripts/NonogramSolver.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+
 namespace Solver
 {
     class NonogramSolver
@@ -22,26 +21,13 @@ namespace Solver
                 if (RevisarSecuenciaValida(tablero, pistas, espacioVacio[0], inicioSequencia, espacioVacio[1],
                     sequenciaSize, ultimaSequenciaFila))
                 {
-                    // Pintar la sequencia
-                    PintarSecuencia(tablero, espacioVacio[0], inicioSequencia, sequenciaSize, 1);
-                    // Como pintar los espacios despues de la sequencia
-                    int columnaInicial = inicioSequencia + sequenciaSize;
-                    if (ultimaSequenciaFila || (ultimaSequenciaFila && columnaInicial != tablero.GetLength(1)))
-                    {
-                        PintarSecuencia(tablero, espacioVacio[0], columnaInicial, tablero.GetLength(1) - columnaInicial, 2);
-                    }
-                    else
-                    {
-                        PintarSecuencia(tablero, espacioVacio[0], columnaInicial, 1, 2);
-                    }
-
                     // Continuar resolviendo
                     if (ResolverNonogram(tablero, pistas, ultimaSequenciaFila ? 0 : pistaFilaActual + 1))
                         return true;
-
-                    // Regresar al estado anterior
-                    PintarSecuencia(tablero, espacioVacio[0], inicioSequencia, tablero.GetLength(1) - inicioSequencia, 0);
                 }
+                // Deshacer la sequencia
+                PintarSecuencia(tablero, espacioVacio[0], inicioSequencia, tablero.GetLength(1) - inicioSequencia, 0);
+
                 // Si la sequencia es del mismo tamanno que la fila
                 if (sequenciaSize == tablero.GetLength(1))
                     return false;
@@ -51,29 +37,26 @@ namespace Solver
             }
             // Limpiar todos los espacios de la fila
             PintarSecuencia(tablero, espacioVacio[0], espacioVacio[1], tablero.GetLength(1) - espacioVacio[1], 0);
-
             return false;
         }
 
         static bool RevisarSecuenciaValida(int[,] tablero, List<List<int[]>> pistas,
                                            int fila, int columna, int columnaOriginal, int size, bool esUltima)
         {
-            // Crear copia del tablero
-            int[,] tableroCopia = tablero.Clone() as int[,];
             // Pintar sequencia
-            PintarSecuencia(tableroCopia, fila, columna, size, 1);
+            PintarSecuencia(tablero, fila, columna, size, 1);
             // Revisar hasta donde se termina de pintar los espacios de la sequencia
             int indexFinalRevision;
             if (esUltima)
             {
                 indexFinalRevision = tablero.GetLength(1);
                 if (columna + size != tablero.GetLength(1))
-                    PintarSecuencia(tableroCopia, fila, columna + size, tablero.GetLength(1) - (columna + size), 2);
+                    PintarSecuencia(tablero, fila, columna + size, tablero.GetLength(1) - (columna + size), 2);
             }
             else
             {
                 indexFinalRevision = columna + size + 1 > tablero.GetLength(1) ? columna + size : columna + size + 1;
-                PintarSecuencia(tableroCopia, fila, columna + size, 1, 2);
+                PintarSecuencia(tablero, fila, columna + size, 1, 2);
             }
             // Probar secuencia por columnas
             for (int i = columnaOriginal; i < indexFinalRevision; i++)
@@ -81,12 +64,12 @@ namespace Solver
                 List<int> sequenciasActuales = new List<int>();
                 int sequenciaActual = 0;
                 bool columnaTerminada = true;
-                for (int j = 0; j < tableroCopia.GetLength(0); j++)
+                for (int j = 0; j < tablero.GetLength(0); j++)
                 {
                     // Agregar a la sequencia actual
-                    if (tableroCopia[j, i] == 1) sequenciaActual++;
+                    if (tablero[j, i] == 1) sequenciaActual++;
                     // Espacio entre sequencias
-                    else if (tableroCopia[j, i] == 2)
+                    else if (tablero[j, i] == 2)
                     {
                         if (sequenciaActual != 0)
                         {
@@ -94,7 +77,7 @@ namespace Solver
                             sequenciaActual = 0;
                         }
                     }
-                    else if (tableroCopia[j, i] == 0)
+                    else if (tablero[j, i] == 0)
                     {
                         columnaTerminada = false;
                         break;
@@ -106,14 +89,19 @@ namespace Solver
                     sequenciasActuales.Add(sequenciaActual);
                     sequenciaActual = 0;
                 }
+                // Revisar si se encontro otra secuencia
                 if (!columnaTerminada && sequenciasActuales.Count == pistas[1][i].Length && sequenciaActual != 0)
                 {
                     return false;
                 }
-
+                // Revisar si la secuencia actual es mayor
+                if (!columnaTerminada && sequenciasActuales.Count < pistas[1][i].Length)
+                {
+                    if (sequenciaActual > pistas[1][i][sequenciasActuales.Count]) return false;
+                }
                 // Verificar numero de 2 y el numero sequencias en la columna
                 if (sequenciasActuales.Count > pistas[1][i].Length ||
-                !RevisarEspaciosColumna(tableroCopia, i, pistas[1][i]))
+                !RevisarEspaciosColumna(tablero, i, pistas[1][i]))
                     return false;
 
                 // Revisar sequencias encontradas
@@ -123,7 +111,7 @@ namespace Solver
                 }
             }
             // Revisar que esten todas las sequencias
-            if (RevisarFilaCompleta(tableroCopia, fila))
+            if (RevisarFilaCompleta(tablero, fila))
             {
                 int totalPistas = 0;
                 for (int i = 0; i < pistas[0][fila].Length; i++)
@@ -131,9 +119,9 @@ namespace Solver
                     totalPistas += pistas[0][fila][i];
                 }
                 int totalCasillasTablero = 0;
-                for (int i = 0; i < tableroCopia.GetLength(1); i++)
+                for (int i = 0; i < tablero.GetLength(1); i++)
                 {
-                    if (tableroCopia[fila, i] == 1) totalCasillasTablero++;
+                    if (tablero[fila, i] == 1) totalCasillasTablero++;
                 }
                 if (totalPistas != totalCasillasTablero) return false;
             }
@@ -155,35 +143,9 @@ namespace Solver
             for (int i = 0; i < size; i++)
             {
                 tablero[fila, columna] = numero;
-                //if (numero == 1)
-                //{
-                //    GridC.setNegro(fila, columna);
-                //}
-                //else if(numero==0)
-                //{
-                //    GridC.setBlanco(fila, columna);
-                //}
-                //else
-                //{
-                //    GridC.setX(fila, columna);
-                //}
                 columna++;
-                
             }
-
         }
-
-        static int GetEspacioDisponibleFila(int[,] tablero, int fila, int[] pistasDeFila)
-        {
-            // Obtener Total de secuencias
-            int resultado = 0;
-            for (int i = 0; i < pistasDeFila.Length; i++)
-            {
-                resultado += pistasDeFila[i];
-            }
-            return tablero.GetLength(1) - resultado;
-        }
-
         static bool HayEspaciosDisponiblesEnFila(int[,] tablero, int fila, int[] pistasDeFila)
         {
             int cantidadEspacios = 0;
