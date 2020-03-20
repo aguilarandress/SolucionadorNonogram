4eea9f5c0166d62da83536f4381f6e1cc743b86f
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-03-19 22:17:11 -0600

    Funcionalidad toggler completada

--- Assets/Scripts/NonoSceneHandler.cs
+++ Assets/Scripts/NonoSceneHandler.cs
@@ -6,70 +6,70 @@ using Solver;
 public class NonoSceneHandler : MonoBehaviour
 {
     private Button startNonogramBtn;
-    private bool animar;
-    public bool empezo;
+    private Toggle animacionToggler;
+    private bool animar = false;
+    public bool empezo = false;
 
     // Start is called before the first frame update
     void Awake()
     {
+
+        Debug.Log(animar);
+        // Obtener game objects
         this.startNonogramBtn = GameObject.Find("ButtonS").GetComponent<Button>();
+        this.animacionToggler = GameObject.Find("animacionToggler").GetComponent<Toggle>();
+        // Set event listeners
         this.startNonogramBtn.onClick.AddListener(Resolver);
+        this.animacionToggler.onValueChanged.AddListener((value) => ToggleAnimacion());
     }
 
-    private void Resolver()
+    // Update is called once per frame
+    void Update()
     {
-        Debug.Log("Empezo...");
-        empezo = true;
-        Thread solveNonogramThread = new Thread(new ThreadStart(ResolverNonogram));
-        solveNonogramThread.Start();
+
     }
 
     void FixedUpdate()
     {
-        if (empezo)
+        // Verificar si se inicio el nonogram y si es animado
+        if (empezo && this.animar)
         {
-
-            if (animar)
+            PintarNonogram();
+            if (DataManager.Instance.termino)
             {
-                // TODO: 
-            }
-            else
-            {
-                // Luego de solucionado el nonogram, se pintan los cuadros acorde al resultado
-                for (int i = 0; i < DataManager.Instance.tablero.GetLength(0); i++)
-                {
-                    string result = "";
-                    for (int j = 0; j < DataManager.Instance.tablero.GetLength(1); j++)
-                    {
-                        result += DataManager.Instance.tablero[i, j].ToString() + " ";
-                        switch (DataManager.Instance.tablero[i, j])
-                        {
-                            case 0:
-                                GridC.setBlanco(i, j);
-                                break;
-                            case 1:
-                                GridC.setNegro(i, j);
-                                break;
-                            default:
-                                GridC.setBlanco(i, j);
-                                break;
-                        }
-                    }
-                    Debug.Log(result);
-                }
-                if (DataManager.Instance.termino)
-                {
-                    empezo = false;
-                }
+                empezo = false;
             }
         }
     }
 
-    static void ResolverNonogram()
+    private void ToggleAnimacion()
     {
-        Debug.Log("xd...");
-        var stopwatch = new System.Diagnostics.Stopwatch();
+        this.animar = !this.animar;
+    }
+
+    private void Resolver()
+    {
+        // Se inicio la solucion
+        empezo = true;
+        // Verificar si debe ser animado
+        if (!this.animar) DataManager.Instance.animado = false;
+        // Iniciar solucion
+        Thread solveNonogramThread = new Thread(new ThreadStart(ResolverNonogram));
+        solveNonogramThread.Start();
+        // Si no es animado esperar a que se termine de resolver
+        if (!this.animar)
+        {
+            solveNonogramThread.Join();
+            PintarNonogram();
+        }
+    }
+
+    void ResolverNonogram()
+    {
+        // Empezar a medir tiempo
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
         stopwatch.Start();
+        // Resolver nonogram
         NonogramSolver.ResolverNonogram(DataManager.Instance.tablero, DataManager.Instance.infoMono, 0);
         stopwatch.Stop();
         Debug.Log(stopwatch.ElapsedMilliseconds.ToString());
@@ -77,12 +77,24 @@ public class NonoSceneHandler : MonoBehaviour
 
     static void PintarNonogram()
     {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Luego de solucionado el nonogram, se pintan los cuadros acorde al resultado
+        for (int i = 0; i < DataManager.Instance.tablero.GetLength(0); i++)
+        {
+            for (int j = 0; j < DataManager.Instance.tablero.GetLength(1); j++)
+            {
+                switch (DataManager.Instance.tablero[i, j])
+                {
+                    case 0:
+                        GridC.setBlanco(i, j);
+                        break;
+                    case 1:
+                        GridC.setNegro(i, j);
+                        break;
+                    default:
+                        GridC.setBlanco(i, j);
+                        break;
+                }
+            }
+        }
     }
 }
